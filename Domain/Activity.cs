cbc8c208f6b7b4b3951f7d2b3400ee5c254bacdc
Cazares_Kh <cazaress2406@gmail.com>
2021-03-30 11:28:43 +0300

    End of section 5

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -12,7 +12,7 @@ namespace Domain
 
         public string Description { get; set; }
 
-        //public string Category { get; set; }
+        public string Category { get; set; }
 
         public string City { get; set; }
 
