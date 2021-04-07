727efc4f8fdf55f12730785041e770a9bf3f6101
Cazares_Kh <cazaress2406@gmail.com>
2021-04-07 20:37:54 +0300

    End of section 9

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -19,6 +19,18 @@ export default class ActivityStore {
     );
   }
 
+  get groupedActivities() {
+    return Object.entries(
+      this.activitiesByDate.reduce((activities, activity) => {
+        const date = activity.date;
+        activities[date] = activities[date]
+          ? [...activities[date], activity]
+          : [activity];
+        return activities;
+      }, {} as { [key: string]: Activity[] })
+    );
+  }
+
   loadActivities = async () => {
     this.loadingInitial = true;
     try {
@@ -42,9 +54,9 @@ export default class ActivityStore {
       try {
         const activity = await agent.Activities.details(id);
         this.setActivity(activity);
-        runInAction(()=>{
+        runInAction(() => {
           this.selectedActivity = activity;
-        })
+        });
         this.setLoadingInitial(false);
         return activity;
       } catch (error) {
