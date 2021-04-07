00e70db729d87d1ae113954c34f6762351836886
Cazares_Kh <cazaress2406@gmail.com>
2021-04-07 15:18:54 +0300

    a typo has fixed

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -25,7 +25,7 @@ export default observer(function ActivityDashboard() {
         <ActivityList />
       </Grid.Column>
       <Grid.Column width="6">
-        <h2>Activity filtes</h2>
+        <h2>Activity filters</h2>
       </Grid.Column>
     </Grid>
   );
