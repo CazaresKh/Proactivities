8778dece53cc9d598911a35e0d82cb7736620b50
Cazares_Kh <cazaress2406@gmail.com>
2021-04-25 08:41:03 +0300

    End of section 15

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,6 +1,6 @@
 import { observer } from "mobx-react-lite";
 import React, { useEffect } from "react";
-import {  useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { Grid } from "semantic-ui-react";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { useStore } from "../../../app/stores/store";
@@ -28,15 +28,15 @@ export default observer(function ActivityDetails() {
     return <LoadingComponent />;
   }
   return (
-   <Grid>
-     <Grid.Column width={10}>
-       <ActivityDetailedHeader activity={activity}/>
-       <ActivityDetailedInfo activity={activity}/>
-       <ActivityDetailedChat/>        
-     </Grid.Column>
-     <Grid.Column width={6}>
-     <ActivityDetailedSideBar/>
-     </Grid.Column>
-   </Grid>
+    <Grid>
+      <Grid.Column width={10}>
+        <ActivityDetailedHeader activity={activity} />
+        <ActivityDetailedInfo activity={activity} />
+        <ActivityDetailedChat />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        <ActivityDetailedSideBar activity={activity} />
+      </Grid.Column>
+    </Grid>
   );
 });
